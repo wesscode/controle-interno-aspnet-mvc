eec4f2fe069534e8d81cfe946a178a76b79e2e04
wess223 <wesley.info07@gmail.com>
2020-01-27 15:05:43 -0300

    um pouco de stylo, data annotations

--- WeW.WEB/Models/Produto.cs
+++ WeW.WEB/Models/Produto.cs
@@ -9,17 +9,21 @@ namespace WeW.WEB.Models
     public class Produto
     {
         [Required]
+        [Display(Name = "Código")]
         public int Cod { get; set; }
 
         [Required]
         public string Nome { get; set; }
 
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
-     
+
+        [Display(Name = "Preço")]
         public double Preco { get; set; }
 
-        public int Quantidade { get; set; }       
+        public int Quantidade { get; set; }
 
+        [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
 
         public Categoria Categoria { get; set; }
